b95a1c28b7bd194e85d032a754bb59f581e0fea0
Mohammed Dawood <raul9978@hotmail.com>
2021-02-10 14:45:45 +0100

    MainMenu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { MainMenu } from './components/MainMenu';
 import { TopMenu } from './components/TopMenu';
 
 function App() {
   return (
     <div className="App">
       <TopMenu/>
+      <MainMenu/>
     </div>
   );
 }
---
+++ src/components/MainMenu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MainMenu = () => {
+    return (
+        <div>
+            <h1>MainMenu</h1>
+        </div>
+    )
+}
