a0d3528e5de0f7a9a81c21e4a7606aea3f006fbc
Mike Blom <blomorama@gmail.com>
2021-02-12 17:36:18 +0100

    Services adjust

--- src/views/Services.js
+++ src/views/Services.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export const Services = () => {
 	return (
-		<div className="my-5 " >
+		<div className="container content py-4" >
+			<h1>About</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac ultricies velit. Vivamus nec velit id metus aliquet dictum. Duis pulvinar faucibus diam, non fermentum nunc suscipit in. Nulla id odio massa. Proin rhoncus massa sem, quis dictum diam eleifend sit amet. Donec lobortis luctus quam quis rhoncus. Pellentesque porta ante nec neque faucibus, eu tincidunt augue commodo.</p>
+			<p>Sed in tortor accumsan, rutrum est non, aliquam felis. Phasellus lacus dui, tristique non lorem at, egestas posuere arcu. Donec eu leo a augue tempor faucibus. In hac habitasse platea dictumst. Quisque elementum libero vitae libero pharetra hendrerit. Sed nec viverra nisl. Nunc feugiat vestibulum nibh, ullamcorper consectetur elit vulputate eu. Nunc bibendum ut massa pulvinar placerat. Aenean nulla nulla, egestas id nisi vitae, rutrum finibus sem. Nunc eros odio, pellentesque eu massa eget, laoreet ullamcorper lacus. Vestibulum eget quam ut nisi tincidunt vulputate. Suspendisse nec mi in libero dignissim semper. Donec quis volutpat odio, ut hendrerit turpis. Ut convallis pharetra nunc.</p>
             <div className="mb-5">
                 <h3 className="text-center" >Our Services</h3>
                 <hr className="underLine w-25 mx-auto" />
