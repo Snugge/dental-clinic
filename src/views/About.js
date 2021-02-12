4b4a2b8cc22bfe53c82177b86634558c79ad503f
Mike Blom <blomorama@gmail.com>
2021-02-12 14:44:55 +0100

    Add Subpages content

--- src/views/About.js
+++ src/views/About.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 export const About = () => {
 	return (
-		<div>
+		<div className="container content py-5">
 			<h1>About</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac ultricies velit. Vivamus nec velit id metus aliquet dictum. Duis pulvinar faucibus diam, non fermentum nunc suscipit in. Nulla id odio massa. Proin rhoncus massa sem, quis dictum diam eleifend sit amet. Donec lobortis luctus quam quis rhoncus. Pellentesque porta ante nec neque faucibus, eu tincidunt augue commodo.</p>
+
+			<p>Sed in tortor accumsan, rutrum est non, aliquam felis. Phasellus lacus dui, tristique non lorem at, egestas posuere arcu. Donec eu leo a augue tempor faucibus. In hac habitasse platea dictumst. Quisque elementum libero vitae libero pharetra hendrerit. Sed nec viverra nisl. Nunc feugiat vestibulum nibh, ullamcorper consectetur elit vulputate eu. Nunc bibendum ut massa pulvinar placerat. Aenean nulla nulla, egestas id nisi vitae, rutrum finibus sem. Nunc eros odio, pellentesque eu massa eget, laoreet ullamcorper lacus. Vestibulum eget quam ut nisi tincidunt vulputate. Suspendisse nec mi in libero dignissim semper. Donec quis volutpat odio, ut hendrerit turpis. Ut convallis pharetra nunc.</p>
+
+			<p>Fusce ultrices, arcu id tempor vehicula, magna elit varius ipsum, ac porttitor sem nulla at velit. Nullam hendrerit, neque et ornare cursus, diam orci rhoncus turpis, et ornare quam sem eu velit. Quisque ac malesuada est. Mauris ac fermentum metus. Sed dui dui, aliquet non risus aliquet, vulputate posuere nunc. Aenean euismod, arcu sed ullamcorper vulputate, dolor sem accumsan odio, a pharetra justo lorem a eros. Sed accumsan sollicitudin sapien. Suspendisse varius neque magna, eget facilisis mi suscipit id. Nulla lacinia nunc in quam molestie tempor. Vestibulum interdum, nulla vitae mattis accumsan, libero velit viverra magna, vel dignissim velit nulla ac nunc. Maecenas tellus nulla, efficitur sit amet convallis at, lobortis quis velit. Pellentesque placerat elit at dolor tincidunt, a semper urna ornare. Ut aliquet ex ut aliquet porta.</p>
+
+			<p>Ut a malesuada diam. Mauris quis felis a felis mattis mattis at sit amet mauris. Quisque interdum pulvinar quam, non porta urna imperdiet eget. Etiam pharetra aliquam placerat. Nulla facilisis fringilla purus in malesuada. Nullam sed tempus velit. Fusce accumsan placerat enim, eu commodo ligula vestibulum sit amet. Pellentesque sem sapien, varius quis efficitur a, vehicula non neque. Duis urna massa, lacinia nec nisi nec, ornare tempor ipsum. Vivamus ut pharetra dolor, non efficitur ex. Nullam quis libero massa. Vestibulum orci diam, consequat eget tortor quis, tempor aliquet risus. Vivamus eget elementum arcu, sit amet maximus lorem. Integer sem lectus, feugiat non lobortis facilisis, bibendum a lorem.</p>
+
+			<p>Maecenas convallis finibus nulla. Praesent vitae pellentesque nisi. Fusce sed velit dapibus, bibendum mi nec, accumsan ligula. Curabitur aliquet et erat quis viverra. Maecenas vulputate nisi ac consectetur rhoncus. Cras convallis pharetra ipsum, ut elementum augue dapibus non. Maecenas tellus nulla, sagittis a elit at, laoreet pulvinar augue.</p>
 		</div>
 	)
 }
